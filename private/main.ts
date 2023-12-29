0f3e3cf694e1bccaf167e9c077439863d9049a8c
Colin Lee <colinlee1129@gmail.com>
2023-12-30 00:17:17 +0800

    updated 1.02

--- private/main.ts
+++ private/main.ts
@@ -1,13 +1,37 @@
 import express from 'express'
-import { Request, Response } from 'express'
+import expressSession from 'express-session'
+import {Client} from 'pg';
+import dotenv from 'dotenv';
 
-const app = express()
+// import path from 'path';
+import { isLoggedIn } from './guard';
 
-app.get('/', function (req: Request, res: Response) {
-  res.end('Hello World')
-})
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const PORT = 8080
+//Make Connection to Database
+dotenv.config();
+
+export const client = new Client({
+  database: process.env.DB_NAME, 
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+});
+
+client.connect();
+
+//set up session
+app.use(
+	expressSession({
+		secret: 'wsp',
+		resave: true,
+		saveUninitialized: true
+	})
+  )
+
+
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}/`)
